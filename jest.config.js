14969737ab1af570b5fa8c45c128afe35c4add90
igor <igorycha.s@yandex.ru>
2026-05-19 11:16:11 +0300

    fix: add skipLibCheck to dom jest tsconfig for CI compatibility

--- jest.config.js
+++ jest.config.js
@@ -58,6 +58,8 @@ module.exports = {
             jsx: 'react-jsx',
             module: 'esnext',
             moduleResolution: 'bundler',
+            skipLibCheck: true,
+            esModuleInterop: true,
           },
         }],
       },
